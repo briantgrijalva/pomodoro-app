b16b46fb95b5526b095299fdfd953a324a6e9963
briantgrijalva <brianygrijalva20@gmail.com>
2022-01-29 10:28:28 -0600

    se agrego un . a img src

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import Clock from './components/Clock';
 import SettingsModal from './components/SettingsModal';
-import logo from './time.png';
+
+import img from './time.png';
 
 
 function App() {
+  console.log(img);
   return (
     <>
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
           <div className="navbar-brand">
-            <img src={logo} alt="" width="30" height="30" className="d-inline-block align-text-top" />
+            <img src={`.${img}`} alt="" width="30" height="30" className="d-inline-block align-text-top" />
             &nbsp; Pomodoro App
           </div>
         </div>
