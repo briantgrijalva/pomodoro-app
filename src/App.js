95471e7e8b13948b203414f617140990cefd572c
briantgrijalva <brianygrijalva20@gmail.com>
2022-01-29 00:26:33 -0600

    se cambio la importacion en <img src />

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Clock from './components/Clock';
 import SettingsModal from './components/SettingsModal';
+import logo from './time.png';
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
           <div className="navbar-brand">
-            <img src={require("./time.png")} alt="" width="30" height="30" className="d-inline-block align-text-top" />
+            <img src={logo} alt="" width="30" height="30" className="d-inline-block align-text-top" />
             &nbsp; Pomodoro App
           </div>
         </div>
