c2a089b62c7d0c46d3bbf83a957b2c20935c89d1
briantgrijalva <brianygrijalva20@gmail.com>
2022-01-28 23:54:51 -0600

    se agrego un . los href y src y se modifico el a href='/'

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
     <>
       <nav className="navbar navbar-light bg-light">
         <div className="container-fluid">
-          <a className="navbar-brand" href="!#">
+          <a className="navbar-brand" href="/">
             <img src={require("./time.png")} alt="" width="30" height="30" className="d-inline-block align-text-top" />
             &nbsp; Pomodoro App
           </a>
